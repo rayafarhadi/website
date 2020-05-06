a813107bcd802b981b1006d630fd5ed039032e4e
Raya Farhadi <raya.farhadi@gmail.com>
2020-05-06 18:58:51 -0400

    email test 2

--- contact.php
+++ contact.php
@@ -1,14 +1,24 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
+$errors="";
 
-    $mailTo = "raya.farhadi@gmail.com";
+$mailTo = "info@rayafarhadi.com";
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+if(empty($name) || empty($mailFrom) || empty($message)){
+    $errors .= "\n Please enter all required fields.";
+}
+
+if(empty($errors)){
+    $body = "Email from my website: \n\n".$message;
     $headers = "From: ".$mailFrom;
-    $txt = "Email from my website: \n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
+
+    header("Location: thank-you.html");
 }
+
+?>
